b9fe189cc69ae639d09be38c4532aa6da39d7e53
SerhijD <sdolnyy@gmail.com>
2021-09-07 18:03:57 +0200

    HomeWork04 - new start with Maven__junit_junit_4_12.xml

---
+++ HomeWork04/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+/**
+ * Homework 04
+ * Sergiy Dolnyy
+ */
+package org.example;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
