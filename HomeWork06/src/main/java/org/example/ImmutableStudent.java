0ecf7af6c04a601b134a7c80bc4d664b69d394ed
serhijd <sdolnyy@gmail.com>
2021-09-12 17:27:30 +0200

    HomeWork 06 - Immutable student - equals() rewritten

--- HomeWork06/src/main/java/org/example/ImmutableStudent.java
+++ HomeWork06/src/main/java/org/example/ImmutableStudent.java
@@ -47,9 +47,16 @@ public final class ImmutableStudent {
             return false;
         }
         ImmutableStudent student = (ImmutableStudent) obj;
+        if (name != null && !name.equals(student.name)) return false;
+        if (group != null && !group.equals(student.group)) return false;
+        if (age != null && age.getYear() != student.age.getYear()) return false;
+        return true;
+        /*
         return (name != null && name.equals(student.getName())) &&
             (group != null && group.equals(student.getGroup())) &&
             (age != null && age.getYear()==student.getAge().getYear());
+
+         */
     }
 
     @Override
