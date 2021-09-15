4b4a12861290ad7dc376b11e06399830ce330b93
Sergiy Dolnyy <sdolnyy@gmail.com>
2021-09-16 01:58:33 +0200

    Car

---
+++ HomeWork08-02/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+/* 2021-09-15
+ * HomeWork 08-02
+ * Sergiy Dolnyy
+ * Create Car
+ */
+package org.example;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "HomeWork 08-02" );
+    }
+}
