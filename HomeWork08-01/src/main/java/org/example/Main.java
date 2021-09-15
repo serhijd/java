78deeecc8b45f8eb9d46ec13cc00a6739e991775
Sergiy Dolnyy <sdolnyy@gmail.com>
2021-09-15 23:20:16 +0200

    Circle added

--- HomeWork08-01/src/main/java/org/example/Main.java
+++ HomeWork08-01/src/main/java/org/example/Main.java
@@ -8,8 +8,7 @@ package org.example;
 
 public class Main
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "HomeWork 08-01" );
         Shape circle = new Circle(3,5,8);
         System.out.println(circle);
