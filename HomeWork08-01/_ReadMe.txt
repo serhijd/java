b323163e3e9cec4ad46327cecdd3d35ad9690e21
Sergiy Dolnyy <sdolnyy@gmail.com>
2021-09-15 21:43:36 +0200

    Triangle

---
+++ HomeWork08-01/_ReadMe.txt
@@ -0,0 +1,6 @@
+2021-09-15
+HomeWork 08-01
+Sergiy Dolnyy
+
+Create triangle based at
+Material/Code samples/OOP samples/08_UseOtherClass
---
+++ HomeWork08-01/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+/* 2021-09-15
+ * HomeWork 08-01
+ * Sergiy Dolnyy
+ * Create Triangle
+ */
+package org.example;
+
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
