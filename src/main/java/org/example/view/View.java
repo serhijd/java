2b726154804a5df2893aaa65f8ba77ab27e67b9d
serhijd <sdolnyy@gmail.com>
2021-09-16 02:30:08 +0200

    Delete src directory

--- src/main/java/org/example/view/View.java
+++
@@ -1,6 +0,0 @@
-package org.example.view;
-
-public class View {
-    public void printMessage(String message){ System.out.print(message); }
-    public void printLnMessage(String message){ System.out.println(message); }
-}
