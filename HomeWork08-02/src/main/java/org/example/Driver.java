edb2f66be63589c748009164a5fe272665dd7390
Sergiy Dolnyy <sdolnyy@gmail.com>
2021-09-16 02:27:49 +0200

    dummy clases created

---
+++ HomeWork08-02/src/main/java/org/example/Driver.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Driver {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private int age;
+    private DrivingLicense drivingLicense;
+}
